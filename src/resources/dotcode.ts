ef86ed6c286b06efa3003834614d3f73df3469da
electrochemistry04 <electrochemistry04@outlook.com>
2022-03-13 20:24:05 +0900

    feat: generate dotcode

---
+++ src/resources/dotcode.ts
@@ -0,0 +1,15 @@
+import bwipjs from "bwip-js";
+
+export const dotcode = (code: string) => {
+  return bwipjs
+    .toBuffer({
+      bcid: "dotcode", // Barcode type
+      text: code, // Text to encode
+      scale: 4,
+      includetext: true,
+      inkspread: 0.25,
+    })
+    .then((png) => {
+      return png;
+    });
+};
