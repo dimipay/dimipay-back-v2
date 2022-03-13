8d9946742c0bf1905d0edb0c849343cbbca6f3a2
electrochemistry04 <electrochemistry04@outlook.com>
2022-03-13 22:26:40 +0900

    refactor: cleancode

--- src/resources/dotcode.ts
+++ src/resources/dotcode.ts
@@ -1,15 +1,11 @@
 import bwipjs from "bwip-js";
 
 export const dotcode = (code: string) => {
-  return bwipjs
-    .toBuffer({
-      bcid: "dotcode", // Barcode type
-      text: code, // Text to encode
-      scale: 4,
-      includetext: true,
-      inkspread: 0.25,
-    })
-    .then((png) => {
-      return png;
-    });
+  return bwipjs.toBuffer({
+    bcid: "dotcode", // Barcode type
+    text: code, // Text to encode
+    scale: 4,
+    includetext: true,
+    inkspread: 0.25,
+  });
 };
