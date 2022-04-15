02399d9814f1a3477dcdca53e2d7e959469660cb
electrochemistry04 <electrochemistry04@gmail.com>
2022-04-15 10:39:27 +0900

    add: get events

--- src/services/notice/controllers.ts
+++ src/services/notice/controllers.ts
@@ -1,10 +1,12 @@
 import { prisma } from "@src/resources";
 import { Request, Response } from "express";
+import { HttpException } from "@src/exceptions";
 
 export const getCurrentNotice = async (req: Request, res: Response) => {
-  const current = new Date();
+  try {
+    const current = new Date();
   return res.json(
-    await prisma.notice.findFirst({
+    await prisma.notice.findMany({
       where: {
         OR: [
           {
@@ -30,4 +32,7 @@ export const getCurrentNotice = async (req: Request, res: Response) => {
       },
     })
   );
+  } catch(e) {
+    throw new HttpException(400, "공지를 조회하는 중 오류가 발생했습니다.");
+  }
 };
