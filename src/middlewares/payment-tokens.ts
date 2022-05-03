f874693a2e2357b9a67212cdd3104489aaff660d
LeeTaekjun <electrochemistry04@outlook.com>
2022-05-03 14:36:17 +0900

    feat: middleware resolving paymentToken

---
+++ src/middlewares/payment-tokens.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+import { HttpException } from "@src/exceptions";
+import bcrypt from "bcrypt";
+import { verifyCustomToken } from "@src/resources";
+import { GeneralPaymentToken, SpecialPaymentToken } from "@src/interfaces";
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  const { token: encryptedToken } = req.body;
+  const { pin, deviceKey, ...token } = (await verifyCustomToken(
+    encryptedToken,
+    req.user.deviceUid
+  )) as GeneralPaymentToken | SpecialPaymentToken;
+  req.body = token;
+
+  if (pin) {
+    if (!bcrypt.compareSync(pin, req.user.paymentPin))
+      throw new HttpException(400, "올바르지 않은 결제 비밀번호입니다.");
+  } else if (deviceKey) {
+    if (!bcrypt.compareSync(deviceKey, req.user.deviceKey))
+      throw new HttpException(400, "올바르지 않은 결제 요청입니다.");
+  } else {
+    throw new HttpException(400, "올바르지 않은 결제 요청입니다.");
+  }
+  next();
+};
