9d54770c6b14d000471b4c83b2bb50bae3789074
LeeTaekjun <electrochemistry04@outlook.com>
2022-05-04 13:04:50 +0900

    refactor: cleancode

--- src/services/user/controllers.ts
+++ src/services/user/controllers.ts
@@ -54,20 +54,21 @@ export const getUserCertkey = async (req: Request, res: Response) => {
       certkey: await rsa.pubkey({ identity: req.user }),
     });
   } catch (e) {
-    throw new HttpException(400, "결제 키 생성 실패");
+    throw new HttpException(400, "결제 암호키 생성 실패");
   }
 };
 
 export const getUserbyApprovalCode = async (req: Request, res: Response) => {
   try {
-    const { approvalCode } = req.body;
+    const { code } = req.body;
 
     const hash = new SHA3(224);
     const redis = await loadRedis();
     const redisKey = key.approvalCode(
-      hash.update(hash.update(approvalCode).digest("hex")).digest("hex")
+      hash.update(hash.update(code).digest("hex")).digest("hex")
     );
     const redisValue = await redis.get(redisKey);
+    redis.del(redisKey);
     if (!redisValue) {
       throw new HttpException(400, "유효하지 않거나 만료된 승인 코드입니다.");
     }
@@ -89,47 +90,6 @@ export const getUserbyApprovalCode = async (req: Request, res: Response) => {
 
     if (user.isDisabled) throw new HttpException(400, "비활성화된 계정입니다.");
     return res.json({ ...user, paymentMethod });
-  } catch (e) {
-    throw new HttpException(e.status, e.message);
-  }
-};
-
-export const getUserbyPurchaseCode = async (req: Request, res: Response) => {
-  try {
-    const { purchaseCode, purchaseType: expectedPurchaseType } = req.body;
-
-    const hash = new SHA3(224);
-    const redis = await loadRedis();
-    const redisKey = key.approvalCode(
-      hash.update(hash.update(purchaseCode).digest("hex")).digest("hex")
-    );
-    const redisValue = await redis.get(redisKey);
-    if (!redisValue) {
-      throw new HttpException(400, "유효하지 않거나 만료된 승인 코드입니다.");
-    }
-
-    const { systemId, paymentMethod, purchaseType } = JSON.parse(redisValue);
-
-    if (expectedPurchaseType !== purchaseType) {
-      throw new HttpException(400, "유효하지 않은 승인 코드입니다.");
-    }
-
-    const user = await prisma.user.findFirst({
-      where: {
-        systemId,
-      },
-      select: {
-        systemId: true,
-        isDisabled: true,
-        name: true,
-        profileImage: true,
-        studentNumber: true,
-        receivedCoupons: true,
-      },
-    });
-
-    if (user.isDisabled) throw new HttpException(400, "비활성화된 계정입니다.");
-    return res.json({ ...user, paymentMethod, purchaseType });
   } catch (e) {
     throw new HttpException(e.status, e.message);
   }
