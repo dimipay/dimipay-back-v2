e7577dad214bfb16fbb5a9c38f56d5f9b96dc606
LeeTaekjun <electrochemistry04@outlook.com>
2022-06-21 22:04:47 +0900

    refactor: is_deleted

--- src/services/product/controllers/getProductByBarcode.ts
+++ src/services/product/controllers/getProductByBarcode.ts
@@ -1,16 +1,27 @@
+import { HttpException } from "@src/exceptions";
 import { prisma } from "@src/resources";
 import { Request, Response } from "express";
 
 export const getProductByBarcode = async (req: Request, res: Response) => {
   const { barcode } = req.params;
 
-  const product = await prisma.product.findFirst({
-    where: {
-      barcode,
-    },
-  });
+  try {
+    const product = await prisma.product.findFirst({
+      where: {
+        AND: {
+          barcode,
+          is_deleted: false,
+        },
+      },
+    });
 
-  res.json({
-    product,
-  });
+    res.json({
+      product,
+    });
+  } catch (e) {
+    if (e instanceof HttpException) {
+      throw new HttpException(e.status, e.message);
+    }
+    throw new HttpException(400, "상품을 찾는 중 오류가 발생했습니다.");
+  }
 };
