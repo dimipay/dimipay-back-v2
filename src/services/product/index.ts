4718b65a277ea8cbfe1aa2ca615954802e4f6faf
LeeTaekjun <electrochemistry04@outlook.com>
2022-06-22 21:49:23 +0900

    refactor: remove barcodeless endpoint

--- src/services/product/index.ts
+++ src/services/product/index.ts
@@ -1,5 +1,5 @@
 import { createService } from "..";
-import { getBarcodelessProducts, getProductByBarcode } from "./controllers";
+import { getProductByBarcode } from "./controllers";
 
 export default createService({
   name: "product",
@@ -7,17 +7,9 @@ export default createService({
   routes: [
     {
       method: "get",
-      path: "/barcodeless",
-      handler: getBarcodelessProducts,
-      needAuth: true,
-      // permission: ["Pos"],
-    },
-    {
-      method: "get",
       path: "/:barcode",
       handler: getProductByBarcode,
       needAuth: true,
-      // permission: ["Pos"],
     },
   ],
 });
